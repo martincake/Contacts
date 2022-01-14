0b65c694663f237fdd75856f5cd48f4e703b5e44
Shilonosov Artyom <av.shilonosov@gmail.com>
2022-01-14 23:28:45 +0500

    Added table view, implemented UITableViewDataSource protocol and cell reuse

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -13,7 +13,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // осуществляем попытку загрузки переиспользуемой ячейки
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") else {
+            print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+            // получаем экземпляр ячейки
+            let newCell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+            // конфигурируем ячейку
+            var configuration = newCell.defaultContentConfiguration()
+            configuration.text = "Строка \(indexPath.row)"
+            newCell.contentConfiguration = configuration
+            return newCell
+        }
+        print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+        // возвращаем сконфигурированный экземпляр ячейки
+        return cell
+    }
+}
